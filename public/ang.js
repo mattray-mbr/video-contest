976f5a62321463422308bfa80217cf872ff61af5
Student <mattray.mbr@gmail.com>
2016-02-18 16:37:07 -0700

    start of video contest project

---
+++ public/ang.js
@@ -0,0 +1,33 @@
+var app = angular.module('myApp', [])
+	app.controller('videoController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
+
+	$scope.$sce = $sce
+	var counter = 0
+
+	//retrieving data from server to be displayed
+	$http.get('/api/videos').then(function(dataFromServer){
+		$scope.collection = dataFromServer.data
+		console.log($scope.collection)
+	})
+
+	//sending form data to server
+	$scope.addNewVideo = function(){
+		counter++
+		if(counter < 9 ){
+			$http.post('/api/videos', $scope.newVideo).then(function(dataFromServer){
+				$scope.collection = dataFromServer.data
+				$scope.newVideo = {}
+			})
+		} else {
+			$scope.test = 'There have Been too many videos submitted'
+			console.log('too may videos')
+		}
+		
+	}
+
+	$scope.vote = function(){
+		$http.post('/')
+	}
+
+
+}])
---
+++ server.js
@@ -0,0 +1,47 @@
+// Requires \\
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+// Create Express App Object \\
+var app = express();
+
+// Application Configuration \\
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+var videos = [
+	// {
+	// 	name: 'first video',
+	// 	url: 'https://www.youtube.com/embed/1RQ7J3QRVmM',
+	// 	title: 'some title',
+	// 	desc: 'stuff and a description',
+	// },
+]
+
+// Routes \\
+app.get('/', function(req, res){
+  res.sendFile('index.html', {root : __dirname+'/public'})
+});
+
+app.get('/api/videos', function(req, res){
+	res.send(videos)
+})
+app.post('/api/videos', function(req, res){
+	console.log('body ->', req.body)
+	videos.push({
+		name: req.body.name,
+		url: req.body.url,
+		title: req.body.title,
+		desc: req.body.desc,
+	})
+	res.send(videos)
+})
+
+// Creating Server and Listening for Connections \\
+var port = 3333
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+})
